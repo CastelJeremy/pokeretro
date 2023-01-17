41f812b45fbf5dcb16375b2c9713c389fccf7303
Enzo <enz.dard@gmail.com>
2023-01-17 20:13:39 +0100

    fix: PostgreSQL column type changed

--- backend/auth/init_database.sql
+++ backend/auth/init_database.sql
@@ -1,2 +1,2 @@
 CREATE DATABASE db_auth;
-CREATE TABLE users (id number CONSTRAINT pk_users PRIMARY KEY, username varchar(64), password varchar(64));
+CREATE TABLE users (id integer CONSTRAINT pk_users PRIMARY KEY, username varchar(64), password varchar(64));
