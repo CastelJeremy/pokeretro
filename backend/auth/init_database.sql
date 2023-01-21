91693997b3edc91c504151e8085da57022097e49
Enzo <enz.dard@gmail.com>
2023-01-21 23:08:13 +0100

    fix: changement des fichiers de config

--- backend/auth/init_database.sql
+++ backend/auth/init_database.sql
@@ -1,2 +1,7 @@
-CREATE DATABASE db_auth;
-CREATE TABLE users (id integer CONSTRAINT pk_users PRIMARY KEY, username varchar(64), password varchar(64));
+CREATE TABLE IF NOT EXISTS public.users
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    username character varying COLLATE pg_catalog."default" NOT NULL,
+    password character varying COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT users_pk PRIMARY KEY (id)
+)
