a84507239057f29ac4e6842bce632c06413028b4
Enzo <enz.dard@gmail.com>
2023-01-21 23:10:55 +0100

    test: Creation de tests unitaires (tester c'est douter, je doutais)

--- backend/auth/src/test/java/net/pokeretro/auth/AuthApplicationTests.java
+++ backend/auth/src/test/java/net/pokeretro/auth/AuthApplicationTests.java
@@ -1,13 +1,37 @@
 package net.pokeretro.auth;
 
+import net.pokeretro.auth.security.TokenManager;
+import net.pokeretro.auth.user.User;
+import net.pokeretro.auth.user.UserRepository;
+import net.pokeretro.auth.user.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class AuthApplicationTests {
 
+    @Autowired
+    private UserService service;
+
     @Test
-    void contextLoads() {
+    void createUser() {
+        User newUser = new User("test4", "password");
+        User savedUser = service.saveUser(newUser);
+
+        assertThat(savedUser).isNotNull();
+        assertThat(savedUser.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    void createToken() {
+        TokenManager tokenManager = TokenManager.getInstance();
+        String token = tokenManager.createToken("test", "test");
+        System.out.println(token);
+        System.out.println(tokenManager.parseToken(token));
+        assertThat(tokenManager.isTokenValid(token)).isEqualTo(0);
     }
 
 }
