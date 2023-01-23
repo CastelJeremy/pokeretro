863c05a23d7af674f7e5eef89b682c304a824c0d
Enzo <enz.dard@gmail.com>
2023-01-23 13:54:14 +0100

    fix: add token blacklist

--- backend/auth/init.sql
+++ backend/auth/init.sql
@@ -4,4 +4,10 @@ CREATE TABLE IF NOT EXISTS public.users
     username character varying COLLATE pg_catalog."default" NOT NULL,
     password character varying COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT users_pk PRIMARY KEY (id)
-)
+);
+
+CREATE TABLE IF NOT EXISTS public.token_blacklist
+(
+    token character varying(200) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT token_blacklist_pkey PRIMARY KEY (token)
+);
