3a076720c6de2015943df83146184389affa5128
CastelJeremy <39135270+CastelJeremy@users.noreply.github.com>
2023-01-28 13:21:31 +0100

    init: spring project

---
+++ backend/battle/src/test/java/net/pokeretro/battle/BattleApplicationTests.java
@@ -0,0 +1,13 @@
+package net.pokeretro.battle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
