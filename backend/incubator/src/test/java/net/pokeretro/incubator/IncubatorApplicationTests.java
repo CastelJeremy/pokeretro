2d2804ff1f3d168a3926bc050f4e4eba40598229
Enzo <enz.dard@gmail.com>
2023-01-31 15:51:54 +0100

    init: spring maven project

---
+++ backend/incubator/src/test/java/net/pokeretro/incubator/IncubatorApplicationTests.java
@@ -0,0 +1,13 @@
+package net.pokeretro.incubator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncubatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
